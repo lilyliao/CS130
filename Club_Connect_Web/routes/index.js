37ecfad65921f00677cbfbb92bdbde4787bd0ef3
Steven Hua <stevenhua1015@gmail.com>
2016-11-20 14:43:30 -0800

    Add basic event feed

--- Club_Connect_Web/routes/index.js
+++ Club_Connect_Web/routes/index.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Kinvey = require('kinvey-node-sdk');
+
+Kinvey.init({
+  appKey: 'kid_HkRu7Qkkl',
+  appSecret: 'fa298122b975486c8ba15d9d8b4c52f5'
+});
+
+var promise = Kinvey.ping().then(function(response) {
+  console.log('Kinvey Ping Success. Kinvey Service is alive, version: ' + response.version + ', response: ' + response.kinvey);
+}).catch(function(error) {
+  console.log('Kinvey Ping Failed. Response: ' + error.description);
+});
 
 module.exports = function (passport) {
 	router.get('/auth/facebook', passport.authenticate('facebook'));
