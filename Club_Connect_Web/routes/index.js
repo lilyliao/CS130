11cac594087c44a35e71f97d336b8c4bbff24b71
Steven Hua <stevenhua1015@gmail.com>
2016-11-20 17:25:20 -0800

    Add Kinvey login and test query

--- Club_Connect_Web/routes/index.js
+++ Club_Connect_Web/routes/index.js
@@ -13,6 +13,65 @@ var promise = Kinvey.ping().then(function(response) {
   console.log('Kinvey Ping Failed. Response: ' + error.description);
 });
 
+
+var promise = new Promise(function(resolve) {
+  resolve(Kinvey.User.getActiveUser());
+});
+promise = promise.then(function onSuccess(user) {
+  if (user) {
+    return user.me();
+  }
+  return user;
+}).then(function onSuccess(user) {
+	console.log(user)
+	if (user==null) {
+		var user = new Kinvey.User();
+		var promise = user.signup({
+		  username: 'username',
+		  password: 'password'
+		}).then(function onSuccess(user) {
+		var dataStore = Kinvey.DataStore.collection('events');
+		var stream = dataStore.find();
+		stream.subscribe(function onNext(entities) {
+			  console.log(entities)
+			}, function onError(error) {
+			  console.log(error)
+			  console.log(error)
+			}, function onComplete() {
+			  console.log(entities)
+			});
+
+		}).catch(function onError(error) {
+		  var promise = Kinvey.User.login('username', 'password').then(function onSuccess(user) {
+		    console.log("logged in");
+		    var dataStore = Kinvey.DataStore.collection('events');
+				var stream = dataStore.find();
+				stream.subscribe(function onNext(entities) {
+					  console.log(entities)
+					}, function onError(error) {
+					  console.log(error)
+					  console.log(error)
+					}, function onComplete() {
+					  console.log(entities)
+				});
+
+		}).catch(function onError(error) {
+		  console.log(error);
+		});
+		});	
+	}
+	else{
+
+	}
+
+}).catch(function onError(error) {
+	console.log(error)
+});
+
+
+
+
+
 var options = {
     timeout:  3000
   , pool:     { maxSockets:  Infinity }
