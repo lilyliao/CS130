a5fbbc243b5d67194cfe6fe9b377114ed718895c
Nikhil Gupta <nikhil2324@g.ucla.edu>
2016-11-20 16:32:42 -0800

    Added routes for individual events

--- Club_Connect_Web/routes/index.js
+++ Club_Connect_Web/routes/index.js
@@ -13,6 +13,14 @@ var promise = Kinvey.ping().then(function(response) {
   console.log('Kinvey Ping Failed. Response: ' + error.description);
 });
 
+var options = {
+    timeout:  3000
+  , pool:     { maxSockets:  Infinity }
+  , headers:  { connection:  "keep-alive" }
+};
+
+var userEvents = []
+
 module.exports = function (passport, graph) {
 	router.get('/auth/facebook', passport.authenticate('facebook'));
 
@@ -22,30 +30,37 @@ module.exports = function (passport, graph) {
 	});
 
 	router.get('/success', function(req, res, next) {
-		var options = {
-		    timeout:  3000
-		  , pool:     { maxSockets:  Infinity }
-		  , headers:  { connection:  "keep-alive" }
-		};
 		graph.setOptions(options).get("/" + passport.session.user.id + "/events", function(err, events) {
 			if (err) {
 				res.end(err);
 			}
 			else {
-		    	console.log(events);
+				userEvents.push.apply(userEvents, events.data);
 			    res.render('dashboard.ejs', {
-				  	user: passport.session.user
+				  	events: events.data,
 				});
 			}
-		  });
+		});
 	});
 
 	router.get('/error', function(req, res, next) {
 	  res.send("Error logging in.");
 	});
 
+	router.get('/events/:id', function (req, res) {
+		var id = req.url.split('/')[2];
+		for (var i = 0; i < userEvents.length; i++) {
+			if (id == userEvents[i].id) {
+				res.render('event.ejs', {
+					event: userEvents[i],
+				});
+			}
+		}
+		res.end("Invalid event id");
+	});
+
 	router.get('/', function(req, res, next) {
-	  res.render('index.ejs');
+		res.redirect('/auth/facebook');
 	});
 
 	return router;
